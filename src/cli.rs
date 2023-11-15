b73881d5f0a72e2a4fbe5ff16cf8001e118ad198
appositum <appositum@pm.me>
2023-11-15 17:00:39 -0300

    separation of concerns
    move main tokio function into `lib.rs`, move command line flag functions
    into `cli.rs`

---
+++ src/cli.rs
@@ -0,0 +1,22 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+
+pub fn flag_append(text: String) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(".gitignore")
+        .unwrap();
+
+    file.write_all(format!("{}\n", text).as_bytes()).unwrap();
+}
+
+pub fn flag_output(text: String, filename: String) {
+    let mut file = File::create(filename).unwrap();
+
+    file.write_all(format!("{}\n", text).as_bytes()).unwrap();
+}
+
+pub fn flag_overwrite(text: String) {
+    flag_output(text, ".gitignore".to_string())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,75 @@
 mod api;
-pub use api::{get_templates, get_template_list};
-
+mod cli;
 mod error;
-pub use error::GIError;
 
-use std::fs::{File, OpenOptions};
-use std::io::Write;
+use error::GIError;
+
+use clap::{App, load_yaml};
+
+#[tokio::main]
+pub async fn run() -> Result<(), GIError> {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    let client = reqwest::Client::new();
+
+    let all_templates: Vec<String> = api::get_template_list(&client).await?;
+
+    if matches.is_present("list") {
+        pretty_print(all_templates);
+
+        return Ok(());
+    }
+
+    if let Some(ts) = matches.value_of("templates") {
+        // this needs to be a vector so we can iterate through the values as references,
+        // that way, the for loop wont consume it. also, we're gonna pass this to
+        // `get_templates`, which takes a vector anyway.
+        let templates_input: Vec<String> = ts.split(',').map(String::from).collect();
+
+        let mut templates_not_found: Vec<String> = Vec::new();
+
+        for t in &templates_input {
+            if !all_templates.contains(t) {
+                templates_not_found.push(t.clone());
+            }
+        }
+
+        if !templates_not_found.is_empty() {
+            return Err(GIError::TemplateNotFound(templates_not_found));
+        }
+
+        let mut output = String::new();
+        let mut print_output = true;
+
+        api::get_templates(&client, templates_input)
+            .await?
+            .into_iter()
+            .for_each(|t| {
+                output.push_str(&format!("### {} ###\n{}", t.name, t.source));
+            });
+
+        if matches.is_present("force") {
+            cli::flag_overwrite(output.clone());
+            print_output = false;
+        } else if matches.is_present("append") {
+            cli::flag_append(output.clone());
+            print_output = false;
+        }
+
+        if let Some(path) = matches.value_of("output") {
+            cli::flag_output(output.clone(), path.to_string());
+            print_output = false;
+        }
+
+        // idk about this lol
+        if print_output {
+            println!("{}", output);
+        }
+    }
+
+    Ok(())
+}
 
 // NOTE: i wonder if there's a prettier way to write this function.
 // the amount of `.clone()` bothers me
@@ -51,24 +115,4 @@ pub fn pretty_print(list: Vec<String>) {
         .for_each(|(x, y, z)| {
             println!("{:<w1$}\t{:<w2$}\t{}", x, y, z, w1 = max1, w2 = max2);
         })
-}
-
-pub fn append(text: String) {
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(".gitignore")
-        .unwrap();
-
-    file.write_all(format!("{}\n", text).as_bytes()).unwrap();
-}
-
-pub fn output(text: String, filename: String) {
-    let mut file = File::create(filename).unwrap();
-
-    file.write_all(format!("{}\n", text).as_bytes()).unwrap();
-}
-
-pub fn overwrite(text: String) {
-    output(text, ".gitignore".to_string())
 }
