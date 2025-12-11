350775987146f92ce04fab4588e95ed83883413d
appositum <appositum@pm.me>
2025-12-11 03:53:25 -0300

    refactor: add newline before template output on `append` command
    separates stuff nicely :)

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,30 @@
 use std::fs::{
+    self,
     File,
     OpenOptions,
 };
 use std::io::Write;
 
 pub fn flag_append(text: String) {
+    let mut append = true;
+
+    if fs::exists(".gitignore").unwrap() {
+        if fs::read_to_string(".gitignore").unwrap().is_empty() {
+            append = false;
+        }
+    } else {
+        append = false;
+    }
+
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
         .open(".gitignore")
         .unwrap();
 
-    file.write_all(text.as_bytes()).unwrap();
+    let formatted_text = if append { format!("\n{}", text) } else { text };
+
+    file.write_all(formatted_text.as_bytes()).unwrap();
 }
 
 pub fn flag_output(text: String, filename: String) {
